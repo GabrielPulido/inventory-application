31587f91368e0603633402b4e6d77ff83b72b0b1
Gabriel Pulido <GabrielPulido0056@gmail.com>
2022-03-08 15:31:30 -0800

    installed luxon and express-validator

---
+++ controllers/itemController.js
@@ -0,0 +1,5 @@
+var Item = require('../models/item');
+var Category = require('../models/category');
+
+var async = require('async');
+const { body, validationResult } = require('express-validator');
