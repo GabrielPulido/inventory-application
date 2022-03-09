f6c2898c5316b8c7df5e58997532f15eacbec528
Gabriel Pulido <GabrielPulido0056@gmail.com>
2022-03-08 16:10:37 -0800

    created homepage layout

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,3 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+//import controllers
+var item_controller = require('../controllers/itemController');
+var category_controller = require('../controllers/categoryController');
+
+//view item
+router.get('/item/:id', item_controller.view_item);
+
+
+
+module.exports = router;
